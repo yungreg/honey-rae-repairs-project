e44eb3f13fb4804409c70de1c39aef1dc210253b
Reggie Williams <rlwjr433@gmail.com>
2022-11-01 11:09:13 -0500

    finish ticket works

--- src/components/tickets/Ticket.js
+++ src/components/tickets/Ticket.js
@@ -24,6 +24,39 @@ if (ticketObject.employeeTickets.length > 0){
 //found the employee profile object for the current USER
 const userEmployee = employees.find(employee => employee.userId === currentUser.id)
 
+
+//todo*: create the function for a button with conditions that only let employees view or use it
+//todo*: add onClick that will run closeTicket on click
+//todo: display the button
+const canCloseTicket = () => {
+    if (userEmployee?.id === assignedEmployee?.id && ticketObject.dateCompleted === "") {
+        return <button className="ticket_finish" 
+        onClick={closeTicket}>Finish Ticket!</button>
+    } else {
+        return ""
+    }
+}
+//todo: create a function that will actuall mark ther ticket closed when thhe finish ticket button is clicked
+    const closeTicket = () => {
+    const copy =  {
+        userId: ticketObject.userId,
+        description: ticketObject.description,
+        emergency: ticketObject.emergency,
+        dateCompleted: new Date()
+    }
+
+    return fetch(`http://localhost:8088/serviceTickets/${ticketObject.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(copy)
+    })
+      .then((res) => res.json())
+      .then(getAllTickets) //youc an pass a fn() reference to .then so it'll just invoke it
+}
+
+
 // modify the button to only show it when an employee is logged in
 const buttonOrNoButton = () => {
     if(currentUser.staff){
@@ -61,12 +94,15 @@ const buttonOrNoButton = () => {
     </header>
     <section>{ticketObject.description}</section>
     <section>Emergency: {ticketObject.emergency ? "🧨" : "No"}</section> 
-    <footer>
+    <footer className="ticket__footer">
         {/* steve's tip: avoid wiriting nested ternary statements. split them into components or functions instead. tip here: https://watch.screencastify.com/v/Noestc1pbJfib5usYjBF */}
         {
             ticketObject.employeeTickets.length
             ? `Currently being worked on by ${assignedEmployee !== null ? assignedEmployee?.user?.fullName :""}`
             : buttonOrNoButton()
+        }
+        {
+            canCloseTicket()
         }
         </footer> 
 </section>
